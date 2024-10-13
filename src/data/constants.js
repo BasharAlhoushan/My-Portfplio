8e3803174c83c02efa572beab64b5d19898d7176
bashar <basharalhuoshan@gmail.com>
2024-10-13 12:54:34 +0300

    Ubdate

--- src/data/constants.js
+++ src/data/constants.js
@@ -409,23 +409,23 @@ export const education = [
 export const projects = [
   {
     id: 11,
-    title: "DecisionHub",
+    title: "InklyReads",
     date: "Jan 2024 - Dec 2023",
     description:
       "A Rule Builder application “Decision Hub” that empowers Business Analysts to create, save, and visualize decision strategies. Provide a no-code rule writing experience and visual representation to test these rules in real-time and observe the calculations at each step.",
     image:
       "https://github.com/rishavchanda/DecisionHub/raw/master/assets/testRule.jpg",
     tags: [
-      "React Js",
-      "PostgressSQL",
-      "Node Js",
-      "Express Js",
-      "Redux",
-      "React Flow",
+      "Html",
+      "Css",
+      "Java Script",
+      "Json Server",
+      "BootStrap",
+      "Figma",
     ],
     category: "web app",
-    github: "https://github.com/rishavchanda/DecisionHub",
-    webapp: "https://decisionhub.netlify.app/",
+    github: "https://github.com/InklyReads/InklyReads",
+    webapp: "https://inklyreads-bashar-alhoushans-projects.vercel.app/",
   },
   {
     id: 9,
